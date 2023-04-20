974641245ced3af0159f8551184c70ac384fc162
yann <yannlefranc330@gmail.com>
2023-04-20 14:28:53 +0200

    Incrementation

---
+++ src/Demo.java
@@ -0,0 +1,46 @@
+public class Demo {
+
+    public static void main( String [] args ) {
+
+        //je déclare mes varaible
+        int thewitcher = 40; // il passe 4 41
+        int thewitcher2 = 25; // il passe a 26
+
+        int result = ++thewitcher + ++thewitcher2; // le ++ rajoute 1 . C'est ce que l'on nomme l'incrementation.
+
+        System.out.println( result );   // Affiche 32
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
